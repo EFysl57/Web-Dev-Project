c1c88ef79f91ad01a4a56e8b4aefe1ad87341260
EFysl57 <195435908+EFysl57@users.noreply.github.com>
2026-04-14 15:01:02 +0500

    product-detail

--- Project/src/product-detail/product-detail.ts
+++ Project/src/product-detail/product-detail.ts
@@ -1,22 +1,25 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../api-service';
 import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef } from '@angular/core';
 @Component({
   selector: 'app-product-detail',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-detail.html',
   styleUrl: './product-detail.css',
 })
 export class ProductDetail {
   product: any;
 
-  constructor(private api: ApiService, private route: ActivatedRoute) {}
+  constructor(private api: ApiService, private route: ActivatedRoute, private cd: ChangeDetectorRef) {}
 
   ngOnInit() {
     const id = this.route.snapshot.params['id'];
 
     this.api.getProduct(id).subscribe((data: any) => {
       this.product = data;
+      this.cd.detectChanges();
     });
   }
 }
